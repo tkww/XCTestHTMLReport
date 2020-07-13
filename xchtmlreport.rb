baf20870d427998fa8db1d507b9c0581bf53e50b
Evan Coleman <e@edc.me>
2020-07-13 18:13:13 -0400

    update homebrew formula

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -1,9 +1,9 @@
 class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
-  homepage "https://github.com/TitouanVanBelle/XCTestHTMLReport"
-  url "https://github.com/TitouanVanBelle/XCTestHTMLReport/archive/2.0.0.tar.gz"
-  sha256 "4424d673d578e84e67fd96afa53a5bd3e80ec7acade65365a123af358c77b47e"
-  head "https://github.com/TitouanVanBelle/XCTestHTMLReport.git", :branch => "develop"
+  homepage "https://github.com/tkww/XCTestHTMLReport"
+  url "https://github.com/tkww/XCTestHTMLReport/archive/2.1.0.zip"
+  sha256 "27a917cfa70b80e1009f40902cf40314baaf30a1418e8ff299847e223885325c"
+  head "https://github.com/tkww/XCTestHTMLReport.git", :branch => "master"
 
   def install
     system "swift build --disable-sandbox -c release"
