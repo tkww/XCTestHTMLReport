330e8b9f0874552589e74394b68376e2171075d1
Evan Coleman <e@edc.me>
2020-07-15 16:32:41 -0400

    update formula

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -1,8 +1,8 @@
 class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/tkww/XCTestHTMLReport"
-  url "https://github.com/tkww/XCTestHTMLReport/archive/2.1.0.zip"
-  sha256 "27a917cfa70b80e1009f40902cf40314baaf30a1418e8ff299847e223885325c"
+  url "https://github.com/tkww/XCTestHTMLReport/archive/2.2.0.zip"
+  sha256 "1c19462a038c276416b3df8276b65c5948b1108f353bea6215a1129d3a7b4272"
   head "https://github.com/tkww/XCTestHTMLReport.git", :branch => "master"
 
   def install
