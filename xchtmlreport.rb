564435b60e603336eb2e53c1ebefcfb9f84657c7
Evan Coleman <e@edc.me>
2020-07-21 15:27:47 -0400

    add workflow

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -1,9 +1,14 @@
 class Xchtmlreport < Formula
-  desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
+  desc "Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/tkww/XCTestHTMLReport"
-  url "https://github.com/tkww/XCTestHTMLReport/archive/2.2.2.zip"
-  sha256 "96e7c2ecb0aad3c340934ba8140ccbe46e21cd7ab7f861f78a4160e33aff7550"
-  head "https://github.com/tkww/XCTestHTMLReport.git", :branch => "master"
+  url "https://github.com/tkww/XCTestHTMLReport/archive/2.2.3.tar.gz"
+  sha256 "990049e044f393a670a693ba54bac7ccfa6c730bb1764a9e231b35356a155151"
+
+  bottle do
+    root_url "https://github.com/tkww/XCTestHTMLReport/releases/download/2.2.3/XCTestHTMLReport.zip"
+    cellar :any_skip_relocation
+    sha256 "3b019e877a3eefdadc2d0f53cfa8e569075f14f43f7cdbbe76c3d4be3246c2b7" => :catalina
+  end
 
   def install
     system "swift build --disable-sandbox -c release"
