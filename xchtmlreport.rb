4d02c7c1b442a32ce8089a9d6516a52425bad4af
Evan Coleman <e@edc.me>
2020-07-21 15:31:48 -0400

    update formula

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -4,12 +4,6 @@ class Xchtmlreport < Formula
   url "https://github.com/tkww/XCTestHTMLReport/archive/2.2.3.tar.gz"
   sha256 "990049e044f393a670a693ba54bac7ccfa6c730bb1764a9e231b35356a155151"
 
-  bottle do
-    root_url "https://github.com/tkww/XCTestHTMLReport/releases/download/2.2.3/XCTestHTMLReport.zip"
-    cellar :any_skip_relocation
-    sha256 "3b019e877a3eefdadc2d0f53cfa8e569075f14f43f7cdbbe76c3d4be3246c2b7" => :catalina
-  end
-
   def install
     system "swift build --disable-sandbox -c release"
     bin.install ".build/release/xchtmlreport"
